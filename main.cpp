5f472a7731a5ae9444c177bd70cdd756e4350dd8
molnarjakab1 <molnarjakab4@gmail.com>
2024-11-09 11:18:27 +0100

    commenting
    commented errors that i'v found

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS NELEMENTS helyett
+    std::cout << '1-100 ertekek duplazasa' // expected ;
+    for (int i = 0;) // for loop szintaktikai hiba int i=0; i<N_ELEMENTS; i++
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // érték üres és expected ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // átlag init 0 és float használata célszerűbb
+    for (int i = 0; i < N_ELEMENTS, i++) //for loop szintaktikai hiba ,->;
     {
-        atlag += b[i]
+        atlag += b[i] //expected ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0; 
+    //dinamikusan lefoglat memóriát törölni kell
 }
